fc658624cc84687d5ab632dd3279767fe97d61c7
tokgozmusa <tokgozmusa@gmail.com>
2018-06-30 23:14:38 +0200

    Add IBOutlet for UILabel

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -9,7 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var recordingLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +23,7 @@ class ViewController: UIViewController {
 
     @IBAction func recordAudio(_ sender: Any) {
         print("record button was pressed")
+        recordingLabel.text = "Recording in progress"
     }
     
 }
