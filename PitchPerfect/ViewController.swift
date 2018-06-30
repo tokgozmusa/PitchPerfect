b6a96080b8832dbebd29e2172693349c4f66c06b
tokgozmusa <tokgozmusa@gmail.com>
2018-06-30 23:07:23 +0200

    Add IBAction for UIButton

--- PitchPerfect/ViewController.swift
+++ PitchPerfect/ViewController.swift
@@ -20,6 +20,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+    }
+    
 }
 
