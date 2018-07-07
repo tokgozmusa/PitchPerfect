feb2a2e029ab5eede083871a12b5a54bd9bee062
tokgozmusa <tokgozmusa@gmail.com>
2018-07-07 14:16:14 +0200

    Call segue programmatically

--- PitchPerfect/RecordSoundsViewController.swift
+++ PitchPerfect/RecordSoundsViewController.swift
@@ -73,6 +73,7 @@ class RecordSoundsViewController: UIViewController, AVAudioRecorderDelegate {
 
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
         print("finished recording")
+        performSegue(withIdentifier: "stopRecording", sender: audioRecorder.url)
     }
 }
 
