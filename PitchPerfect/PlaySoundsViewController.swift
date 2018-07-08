349513419640a6ada6fbf00bdf2806fca347fa40
tokgozmusa <tokgozmusa@gmail.com>
2018-07-08 13:24:39 +0200

    Rename variables

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -9,10 +9,10 @@
 import UIKit
 
 class PlaySoundsViewController: UIViewController {
-    @IBOutlet weak var snailButton: UIButton!
-    @IBOutlet weak var chipmunkButton: UIButton!
-    @IBOutlet weak var rabbitButton: UIButton!
-    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var slowButton: UIButton!
+    @IBOutlet weak var fastButton: UIButton!
+    @IBOutlet weak var highPitchButton: UIButton!
+    @IBOutlet weak var lowPitchButton: UIButton!
     @IBOutlet weak var echoButton: UIButton!
     @IBOutlet weak var reverbButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
