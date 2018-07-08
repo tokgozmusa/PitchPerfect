842d43adb532b3ed9be53532b761896f005eb64f
tokgozmusa <tokgozmusa@gmail.com>
2018-07-08 13:20:18 +0200

    Add IBOutlets

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class PlaySoundsViewController: UIViewController {
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
     var recordedAudioURL: URL!
 
     override func viewDidLoad() {
