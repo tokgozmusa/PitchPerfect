c4f81b0534737ba538db2fac38bcc997183c28ae
tokgozmusa <tokgozmusa@gmail.com>
2018-07-08 14:04:50 +0200

    Call stop audio function

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -51,6 +51,7 @@ class PlaySoundsViewController: UIViewController {
     
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
         print("Stop Audio Button Pressed")
+        stopAudio()
     }
 
     override func viewDidLoad() {
